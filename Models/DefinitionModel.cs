21887169b2af8fc46121119f9d404419041597d9
Corina <corina@fate.org.au>
2024-09-19 20:50:12 +1000

    added numbers and occupations

--- Models/DefinitionModel.cs
+++ Models/DefinitionModel.cs
@@ -9,8 +9,23 @@ public class DefinitionWithImage : Definition
   public required string Image { get; set; }
 }
 
-public class HomeDefinitionWithImage : Definition
+public class DefinitionWithImageLink : Definition
 {
   public required string Image { get; set; }
   public required string Target { get; set; } 
+}
+
+public class DefinitionWithNumber
+{
+  public required string EnglishNumber { get; set; }
+  public required string PolishNumber { get; set; }
+  public required string PolishAge { get; set; }
+  public required string PolishMoney { get; set; }
+}
+
+public class DefinitionPolish
+{
+    public required string Male { get; set; }
+    public required string Female { get; set; }
+    public required string Image { get; set; }
 }
